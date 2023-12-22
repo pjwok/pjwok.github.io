1ce93c1665b8df1827c1f983def59d9fbd34be87
pjwok <523967800@qq.com>
2023-12-22 13:06:59 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -65,7 +65,13 @@ function startRecording() {
 			Create the Recorder object and configure to record mono sound (1 channel)
 			Recording 2 channels  will double the file size
 		*/
-		rec = new Recorder(input,{numChannels:1})
+		rec = new Recorder(input,{
+			numChannels:1,
+			type:"wav",   //此处的type类型是可修改的
+			bitRate:128,      
+			sampleRate:16000,
+			// bufferSize:8192,
+		})
 
 		//start the recording process
 		rec.record()
@@ -111,57 +117,20 @@ function stopRecording() {
 	//stop microphone access
 	gumStream.getAudioTracks()[0].stop();
 
-	//create the wav blob and pass it on to createDownloadLink
-	rec.exportWAV(createDownloadLink);
+	//create the wav blob and pass it on to convertBase64
+	rec.exportWAV(convertBase64);
 }
 
-function createDownloadLink(blob) {
+function convertBase64(blob) {
+
+	const reader = new FileReader();
+
+	reader.onload = function(event) {
+		const base64String = event.target.result;
+		console.log('base64String==',base64String);
+		document.write(base64String)
+	};
+
+	reader.readAsDataURL(blob);
 	
-	var url = URL.createObjectURL(blob);
-	var au = document.createElement('audio');
-	var li = document.createElement('li');
-	var link = document.createElement('a');
-
-	//name of .wav file to use during upload and download (without extendion)
-	var filename = new Date().toISOString();
-
-	//add controls to the <audio> element
-	au.controls = true;
-	au.src = url;
-
-	//save to disk link
-	link.href = url;
-	link.download = filename+".wav"; //download forces the browser to donwload the file using the  filename
-	link.innerHTML = "Save to disk";
-
-	//add the new audio element to li
-	li.appendChild(au);
-	
-	//add the filename to the li
-	li.appendChild(document.createTextNode(filename+".wav "))
-
-	//add the save to disk link to li
-	li.appendChild(link);
-	
-	//upload link
-	var upload = document.createElement('a');
-	upload.href="#";
-	upload.innerHTML = "Upload";
-	upload.addEventListener("click", function(event){
-		  var xhr=new XMLHttpRequest();
-		  xhr.onload=function(e) {
-		      if(this.readyState === 4) {
-		          console.log("Server returned: ",e.target.responseText);
-		      }
-		  };
-		  var fd=new FormData();
-		  fd.append("audio_data",blob, filename);
-		  xhr.open("POST","upload.php",true);
-		  xhr.send(fd);
-	})
-	li.appendChild(document.createTextNode (" "))//add a space in between
-	li.appendChild(upload)//add the upload link to li
-
-	//add the li element to the ol
-	recordingsList.appendChild(li);
 }
